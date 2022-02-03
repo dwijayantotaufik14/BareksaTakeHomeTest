ea7d703e7fb6584d39eec02cdf899a1f820e40c0
opick <opick@impacto.id>
2022-02-03 11:55:26 +0700

    RESTful API, MVVM

---
+++ BareksaTakeHomeTest/BareksaTakeHomeTest/Dashboard/View/Cell/CompanyCollectionCell.swift
@@ -0,0 +1,17 @@
+//
+//  CompanyCollectionCell.swift
+//  BareksaTakeHomeTest
+//
+//  Created by Brilliann Nuswantara Bhagawanta on 28/01/22.
+//
+
+import UIKit
+
+class CompanyCollectionCell: UICollectionViewCell {
+    
+    @IBOutlet var companyImageView: UIImageView!
+    @IBOutlet var companyName: UILabel!
+    
+    
+    
+}
